c70b3483697d8abfd3be429fc678b0eacf7f1b99
Kai O Brien <karenelizabeth28597@gmail.com>
2025-11-18 21:30:18 +0100

    tidied up redundant code

--- tracker.py
+++ tracker.py
@@ -4,18 +4,11 @@ import pymongo
 from urllib.parse import quote
 
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 uri = MONGODB_URI
 # Create a new client and connect to the server
 client = MongoClient(uri)
 db = client['tv_show_tracker']  # Database name
 shows_collection = db['shows']
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
 
 
 def get_show(show_name: str) -> dict | None:
